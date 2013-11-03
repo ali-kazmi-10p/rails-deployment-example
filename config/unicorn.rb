fcdc8f0db40e3544aa057b3bfc76e21da1848215
shinokada <okada.shin@gmail.com>
2013-11-03 21:30:10 +0900

    first commit after clone

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,3 +5,17 @@ timeout 30
 pid "/tmp/unicorn_rails3demo.pid"
 # stdout_path "/vagrant/log/unicorn.log"
 # stderr_path "/vagrant/log/unicorn.log"
+
+
+
+
+# working_directory File.expand_path("../..", __FILE__)
+# # APP_PATH = "/vagrant"
+# # working_directory APP_PATH
+# worker_processes 2
+# listen "/tmp/unicorn.sock"
+# timeout 30
+# # pid "/vagrant/tmp/pids/unicorn.pid"
+# pid "/tmp/unicorn_rails3demo.pid"
+# # stdout_path "/vagrant/log/unicorn.log"
+# # stderr_path "/vagrant/log/unicorn.log"
